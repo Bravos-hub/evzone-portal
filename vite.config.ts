d883522606b448d26dfd9ab6919a6170a0e6e55f
BRAVE OLIMI <bolimi@bugemauniv.ac.ug>
2026-01-02 12:45:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+mport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    host: true, // allows access via domain / LAN
+    allowedHosts: ['portal.evzonecharging.com'],
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['portal.evzonecharging.com'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 })
-
